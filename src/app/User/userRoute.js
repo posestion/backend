28f1f9f0acf73814adc3994e18b31516bf32f748
yein <yenqkr@naver.com>
2023-07-11 15:51:36 +0900

    provider 삭제

--- src/app/User/userRoute.js
+++ src/app/User/userRoute.js
@@ -2,5 +2,6 @@ module.exports = function (app) {
   const user = require("./userController");
   const jwtMiddleware = require("../../../config/jwtMiddleware");
 
+  // 중복 id 검사
+  app.get("/app/users:id", user.repeatId);
 };
-
