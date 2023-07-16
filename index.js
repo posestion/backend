c0cfcaed1606ff78b762a61a9b1d0663d5c516aa
RhoSeungA <sskclara@naver.com>
2023-07-16 20:50:01 +0900

    배포를 위해 index.js , package.json 수정

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('./config/express');
 const {logger} = require('./config/winston');
-
+const http = require('http');
+const app = express();
 const port = 3000;
-express().listen(port);
+
+//express().listen(port); -> 배포를 위해 수정
+http.createServer(app).listen(process.env.PORT || 3000);
+
 logger.info(`${process.env.NODE_ENV} - API Server Start At Port ${port}`);
